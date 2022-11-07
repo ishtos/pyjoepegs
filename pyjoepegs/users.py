e103ac49fbb21ac7b6bd10da57afeb4930b00ff7
ishtos <ginkgonut.9960@gmail.com>
2022-11-08 01:02:27 +0900

    feature: users api

--- pyjoepegs/users.py
+++ pyjoepegs/users.py
@@ -11,4 +11,37 @@ from .base import BaseAPI
 
 
 class UsersAPI(BaseAPI):
-    ...
+    def get_user_by_name(self, name, **params):
+        endpoint = f"/v2/users/name/{name}"
+
+        return self.request(endpoint, params=params)
+
+    def get_user(self, address, **params):
+        endpoint = f"/v2/users/{address}"
+
+        return self.request(endpoint, params=params)
+
+    def get_user_collections(self, address, **params):
+        endpoint = f"/v2/users/{address}/collections"
+
+        return self.request(endpoint, params=params)
+
+    def get_user_items(self, address, **params):
+        endpoint = f"/v2/users/{address}/items"
+
+        return self.request(endpoint, params=params)
+
+    def get_user_items_on_auction(self, address, **params):
+        endpoint = f"/v2/users/{address}/auction-items"
+
+        return self.request(endpoint, params=params)
+
+    def get_user_bids_received(self, address, **params):
+        endpoint = f"/v2/users/{address}/bids-received"
+
+        return self.request(endpoint, params=params)
+
+    def get_user_activity(self, address, **params):
+        endpoint = f"/v2/users/{address}/activity"
+
+        return self.request(endpoint, params=params)
---
+++ tests/test_users.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""
+@File    :   test_users.py
+@Time    :   2022/11/07 23:55:04
+@Author  :   ishtos
+@License :   (C)Copyright 2022 ishtos
+"""
+
+import os
+import unittest
+
+import dotenv
+
+from pyjoepegs.users import UsersAPI
+
+
+class TestActivitiesAPI(unittest.TestCase):
+    def setUp(self) -> None:
+        dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+        dotenv.load_dotenv(dotenv_path=dotenv_path)
+        api_key = os.environ.get("API_KEY")
+
+        self.api = UsersAPI(api_key=api_key)
+
+    # @unittest.skip("TODO: 422 Unprocessable Entity")
+    def test_get_user_by_name(self) -> None:
+        name = "ishtos"
+        response = self.api.get_user_by_name(name)
+
+        self.assertEqual(response["status_code"], 200)
+
+    def test_get_user(self) -> None:
+        address = "0xb842344669579ecf4cee12f740520376c4cbc6d1"
+        response = self.api.get_user(address)
+
+        self.assertEqual(response["status_code"], 200)
+
+    def test_get_user_collections(self) -> None:
+        address = "0xb842344669579ecf4cee12f740520376c4cbc6d1"
+        response = self.api.get_user_collections(address)
+
+        self.assertEqual(response["status_code"], 200)
+
+    def test_get_user_items(self) -> None:
+        address = "0xb842344669579ecf4cee12f740520376c4cbc6d1"
+        response = self.api.get_user_items(address)
+
+        self.assertEqual(response["status_code"], 200)
+
+    def test_get_user_items_on_auction(self) -> None:
+        address = "0xb842344669579ecf4cee12f740520376c4cbc6d1"
+        response = self.api.get_user_items_on_auction(address)
+
+        self.assertEqual(response["status_code"], 200)
+
+    def test_get_user_bids_received(self) -> None:
+        address = "0xb842344669579ecf4cee12f740520376c4cbc6d1"
+        response = self.api.get_user_bids_received(address)
+
+        self.assertEqual(response["status_code"], 200)
+
+    def test_get_user_activity(self) -> None:
+        address = "0xb842344669579ecf4cee12f740520376c4cbc6d1"
+        response = self.api.get_user_activity(address)
+
+        self.assertEqual(response["status_code"], 200)
